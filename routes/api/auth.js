626b0124976e848ad8c2e4b3e1ba57893627b481
Milan Sony <milansonyofficial@gmail.com>
2025-05-06 17:36:16 +0530

    style: req type changed

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { signup, login, refresh, logout, profile, checkAuth } from "../../controllers/authControllers.js"
+import { signup, login, refresh, logout, profile } from "../../controllers/authControllers.js"
 import { verifyToken } from "../../middleware/verifyToken.js"
 
 const authRoutes = express.Router()
@@ -11,16 +11,12 @@ authRoutes.post("/signup", signup)
 authRoutes.post("/login", login)
 
 // generate refresh token
-authRoutes.post("/refresh", refresh)
+authRoutes.get("/refresh", refresh)
 
 // user logout
-authRoutes.post("/logout", logout)
+authRoutes.get("/logout", logout)
 
 // user profile
-authRoutes.post("/profile", profile)
-
-// check auth
-authRoutes.post("/check", verifyToken, checkAuth)
-
+authRoutes.post("/profile", verifyToken, profile)
 
 export default authRoutes
