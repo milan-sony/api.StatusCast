0dd706203be8fe1dbb5b456541a1faf70b97f90f
Milan Sony <milansonyofficial@gmail.com>
2025-06-15 19:43:16 +0530

    feat: added the functionality for sending the friend request

--- controllers/friendRequestController.js
+++ controllers/friendRequestController.js
@@ -4,9 +4,6 @@ import User from '../model/userModel.js'
 export const sendFriendRequest = async (req, res) => {
     try {
         const from = req.user?._id
-
-        console.log("from: ", from)
-
         const { to } = req.body
 
         if (from === to) {
@@ -56,36 +53,6 @@ export const sendFriendRequest = async (req, res) => {
                 error
             }
         )
-    }
-}
-
-// Accept/Reject Friend Request
-export const respondToFriendRequest = async (req, res) => {
-    try {
-        const { requestId, action } = req.body
-        const request = await FriendRequest.findById(requestId)
-
-        if (!request || request.to.toString() !== req.user._id) {
-            return res.status(404).json({ message: "Request not found or unauthorized" })
-        }
-
-        if (!['accepted', 'rejected'].includes(action)) {
-            return res.status(400).json({ message: "Invalid action" })
-        }
-
-        request.status = action
-        await request.save()
-
-        if (action === 'accepted') {
-            await User.findByIdAndUpdate(request.from, { $push: { friends: request.to } })
-            await User.findByIdAndUpdate(request.to, { $push: { friends: request.from } })
-        }
-
-        res.status(200).json({ message: `Friend request ${action}.` })
-
-    } catch (error) {
-        console.error("Respond Error:", error)
-        res.status(500).json({ message: "Something went wrong", error })
     }
 }
 
--- routes/api/friendRequest.js
+++ routes/api/friendRequest.js
@@ -1,10 +1,9 @@
 import express from 'express'
-import { sendFriendRequest, respondToFriendRequest } from '../../controllers/friendRequestController.js'
+import { sendFriendRequest } from '../../controllers/friendRequestController.js'
 import { verifyToken } from '../../middleware/verifyToken.js'
 
 const friendRequestRoutes = express.Router()
 
 friendRequestRoutes.post('/send', verifyToken, sendFriendRequest)
-friendRequestRoutes.post('/respond', verifyToken, respondToFriendRequest)
 
 export default friendRequestRoutes
