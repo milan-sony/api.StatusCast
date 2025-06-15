98153bba86183a9214a8086e696e63093f54e216
Milan Sony <milansonyofficial@gmail.com>
2025-06-15 15:00:13 +0530

    fix: schema updated

--- model/statusModel.js
+++ model/statusModel.js
@@ -1,33 +1,34 @@
 import mongoose from "mongoose";
 
-const statusSchema = new mongoose.Schema({
-    userId: {
-        type: String,
-        ref: 'User',
-        required: true
-    },
-    emoji: {
-        type: String,
-        required: true,
-    },
-    status: {
-        type: String,
-        required: true
-    },
-    startTime: {
-        type: String,
-        required: true
-    },
-    endTime: {
-        type: String,
-        required: true
-    }
-},
+const statusSchema = new mongoose.Schema(
     {
-        timestamps: true
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        emoji: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        },
+        startTime: {
+            type: Date,
+            required: true,
+        },
+        endTime: {
+            type: Date,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
     }
-)
+);
 
-const Status = mongoose.model('Status', statusSchema)
+const Status = mongoose.model('Status', statusSchema);
 
-export default Status
+export default Status;
